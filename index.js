4ff3c0ff47c9b5d3ffd1c68a7f511d7302fb3fed
Clarise Nicole Smith <36603779+Fillygroove@users.noreply.github.com>
2022-09-11 18:45:26 -0400

    drawing fixes

--- index.js
+++ index.js
@@ -1,50 +1,13 @@
-function print(arr) {
-    let out = ''
-    for (let i = 0; i < arr.length; i++) out += arr[i].toString().replaceAll(',', '\t') + '\n';
-    console.log(out);
-}
-
-let dims = {
-    width: 10,
-    height: 20,
-    size: 500
-};
-
-let wrapper = document.getElementById('wrapper');
-let divBoard = document.getElementById('board');
-let pauseDiv = document.getElementById('pause');
-let wrapperWidth = dims.size * dims.width / dims.height;
-let wrapperHeight = dims.size;
-
-wrapper.style.width = `${wrapperWidth}px`;
-wrapper.style.height = `${wrapperHeight}px`;
-
-for (let i = 0; i < dims.height; i++) {
-    let row = document.createElement('div');
-    row.className = 'game-row';
-    row.id = `row${i}`;
-    row.style.height = `${100 / dims.height}%`;
-
-    for (let j = 0; j < dims.width; j++) {
-        let col = document.createElement('div');
-        col.className = 'game-col';
-        col.id = `col${j}`;
-        col.style.width = `${100 / dims.width}%`;
-
-        row.append(col);
-    }
-
-    divBoard.append(row);
-}
-
-let board = ((out = []) => {
-    for (let i = 0; i < dims.height; i++) out.push(new Array(dims.width).fill(undefined));
-    return out;
-})();
-let piece;
-
-let minoes = {
-    i: {
+let game = {
+    done: undefined,
+    level: 0,
+    lines: 0,
+    timer: 0,
+    speed: 2,
+    linesCleared: [],
+    placeBuffer: 0,
+    paused: false,
+    minoes: [{
         name: 'I',
         color: '#00F0F1',
         shape: [
@@ -53,8 +16,7 @@ let minoes = {
             [0, 0, 0, 0],
             [0, 0, 0, 0]
         ]
-    }, 
-    j: {
+    }, {
         name: 'J',
         color: '#0000f0',
         shape: [
@@ -62,8 +24,7 @@ let minoes = {
             [1, 1, 1],
             [0, 0, 0],
         ]
-    }, 
-    l: {
+    }, {
         name: 'L',
         color: '#EF9F00',
         shape: [
@@ -71,8 +32,7 @@ let minoes = {
             [1, 1, 1],
             [0, 0, 0],
         ]
-    }, 
-    s: {
+    }, {
         name: 'S',
         color: '#01F001',
         shape: [
@@ -80,8 +40,7 @@ let minoes = {
             [1, 1, 0],
             [0, 0, 0]
         ]
-    },
-    z: {
+    }, {
         name: 'Z',
         color: '#F00001',
         shape: [
@@ -89,8 +48,7 @@ let minoes = {
             [0, 1, 1],
             [0, 0, 0]
         ]
-    }, 
-    t: {
+    }, {
         name: 'T',
         color: '#A000F0',
         shape: [
@@ -98,16 +56,14 @@ let minoes = {
             [1, 1, 1],
             [0, 0, 0]
         ]
-    }, 
-    o: {
+    }, {
         name: 'O',
         color: '#F1F000',
         shape: [
             [1, 1],
             [1, 1]
         ]
-    }/*, 
-    o: { // This version of the o piece allows for o-spins
+    }/*, { // This version of the o piece allows for o-spins
         name: 'O',
         color: '#F1F000',
         shape: [
@@ -115,8 +71,7 @@ let minoes = {
             [0, 1, 1],
             [0, 0, 0]
         ]
-    }*//*,
-    wtf: {
+    }*//*, {
         name: 'wtf',
         color: '#F0007F',
         shape: [
@@ -126,55 +81,91 @@ let minoes = {
             [1, 1, 0, 0, 0],
             [1, 0, 0, 0, 0]
         ]
-    }*/
+    }*/],
+    dims: {
+        width: 10,
+        height: 20
+    },
+    control: {
+        hdrop: {
+            key: 'w',
+            pressed: 0,
+            buffer: 0
+        },
+        down: {
+            key: 's',
+            pressed: 0
+        },
+        left: {
+            key: 'a',
+            pressed: 0
+        },
+        right: {
+            key: 'd',
+            pressed: 0
+        },
+        ccw: {
+            key: 'j',
+            pressed: 0,
+            buffer: 0
+        },
+        cw: {
+            key: 'k',
+            pressed: 0,
+            buffer: 0
+        },
+        pause: {
+            key: 'Enter',
+            pressed: 0,
+            buffer: 0
+        }
+    }
 };
 
-let minoArray = Object.values(minoes);
+let boardSize = 500;
+let wrapper = document.getElementById('wrapper');
+let divBoard = document.getElementById('board');
+let pauseDiv = document.getElementById('pause');
+let wrapperWidth = boardSize * game.dims.width / game.dims.height;
+let wrapperHeight = boardSize;
 
-function shapeTemplate(shape) {
-    let out = [];
-    
-    for (let i = 0; i < shape.length; i++) {
-        out.push(new Array(shape.length));
+wrapper.style.width = `${wrapperWidth}px`;
+wrapper.style.height = `${wrapperHeight}px`;
+
+for (let i = 0; i < game.dims.height; i++) {
+    let row = document.createElement('div');
+    row.className = 'game-row';
+    row.id = `row${i}`;
+    row.style.height = `${100 / game.dims.height}%`;
+
+    for (let j = 0; j < game.dims.width; j++) {
+        let col = document.createElement('div');
+        col.className = 'game-col';
+        col.id = `col${j}`;
+        col.style.width = `${100 / game.dims.width}%`;
+
+        row.append(col);
+    }
+
+    divBoard.append(row);
+}
+
+let board = ((out = []) => {
+    for (let i = 0; i < game.dims.height; i++) {
+        out.push(new Array(game.dims.width).fill(undefined));
     }
     
     return out;
+})();
+let piece;
+
+function print(arr) {
+    let out = ''
+    for (let i = 0; i < arr.length; i++) out += arr[i].toString().replaceAll(',', '\t') + '\n';
+    console.log(out);
 }
 
-function rotatePiece(dir, check = 0) {
-    let newIndices = [];
-
-    for (let i = 0; i < piece.indices.length; i++) {
-        let y = piece.indices[i][0] - piece.center[0];
-        let x = piece.indices[i][1] - piece.center[1];
-        
-        let scale = Math.sqrt(y * y + x * x);
-        let angle = Math.atan2(x, y);
-
-        let indexArray = [
-            Math.round(scale * Math.cos(angle - dir * Math.PI / 2) + piece.center[0]), 
-            Math.round(scale * Math.sin(angle - dir * Math.PI / 2) + piece.center[1])
-        ];
-
-        if (
-            // attempted location exists
-            board[indexArray[0]][indexArray[1]] ||
-            // out of bounds
-            indexArray[0] < 0 || indexArray[0] > dims.height -1 ||
-            indexArray[1] < 0 || indexArray[1] > dims.width -1 ||
-            // piece did not pass SRS tests
-            check > 4
-        ) {
-            // Implement TTC SRS by adjusting the centerpoint, allowing for four extra checks
-            return;
-        }
-
-        newIndices.push(indexArray);
-    }
-
-    piece.indices = newIndices;
-}
-
+// Literally just stolen from stackoverflow lol
 function shadeColor(color, percent) {
     var R = parseInt(color.substring(1,3),16);
     var G = parseInt(color.substring(3,5),16);
@@ -195,15 +186,17 @@ function shadeColor(color, percent) {
     return "#"+RR+GG+BB;
 }
 
-function colorSquare(col, row, color) {
+function drawPixel(col, row, color) {
+    // Find the pixel and give it proper attributes
     let pixel = divBoard.children[col].children[row];
     pixel.className = 'game-col';
-    pixel.style.width = `${100 / dims.width}%`;
-    pixel.style.height = `100%`;
+    pixel.style.width = `${100 / game.dims.width}%`;
 
+    // Kill the child.
     pixel.innerHTML = '';
     pixel.style.backgroundColor = color;
 
+    // Utter jank. I should base it off of tetromino index and not color.
     if (color != '#333333') {
         if (pixel.children.length == 0) {
             let mino = document.createElement('div');
@@ -217,74 +210,21 @@ function colorSquare(col, row, color) {
     }
 }
 
-function eraseBoard() {
-    for (let i = 0; i < dims.height; i++) {
-        for (let j = 0; j < dims.width; j++) {
-            colorSquare(i, j, '#333333');
-        }
+function erasePiece() {
+    for (let indices of piece.indices) {
+        drawPixel(indices[0], indices[1], '#333333');
     }
 }
 
-function drawBoard() {
-    for (let i = 0; i < dims.height; i++) {
-        for (let j = 0; j < dims.width; j++) {
-            if (board[i][j]) {
-                colorSquare(i, j, board[i][j]);
-            } else {
-                colorSquare(i, j, '#333333');
-            }
-        }
-    }
-    if (piece) {
-        for (let i = 0; i < piece.indices.length; i++) {
-            colorSquare(piece.indices[i][0], piece.indices[i][1], piece.color);
-        }
+function drawPiece() {
+    for (let indices of piece.indices) {
+        drawPixel(indices[0], indices[1], piece.color);
     }
 }
 
-function movePiece(x, y) {
-    let newIndices = [];
-
-    for (let i = 0; i < piece.indices.length; i++) {
-        let newY = piece.indices[i][0] + y;
-        let newX = piece.indices[i][1] + x;
-
-        // to-do: make it so that only downward collisions create a new piece
-
-        if (!board[newY] || board[newY][newX]) {
-            if (y != 0) {
-                if (game.placeBuffer) {
-                    for (let i = 0; i < piece.indices.length; i++) {
-                        board[piece.indices[i][0]][piece.indices[i][1]] = piece.color;
-                        if (!board[piece.indices[i][0]].includes(undefined)) {
-                            if (!game.linesCleared.includes(piece.indices[i][0])) {
-                                game.linesCleared.push(piece.indices[i][0]);
-                            }
-                        }
-                    }
-                    piece = undefined;
-                    game.placeBuffer--;
-                } else {
-                    game.placeBuffer = 1;
-                }
-            }
-            return;
-        } else if (newX < 0 || newX > dims.width - 1) {
-            return;
-        }
-        newIndices.push([newY, newX]);
-    }
-
-    if (game.placeBuffer) game.placeBuffer--;
-
-    piece.indices = newIndices;
-    piece.center[0] += y;
-    piece.center[1] += x;
-    drawBoard();
-}
-
-function setPiece(mino) {
-    let leftmost = Math.floor((dims.width - mino.shape.length) / 2);
+function setPiece(minoIndex) {
+    let mino = game.minoes[minoIndex];
+    let leftmost = Math.floor((game.dims.width - mino.shape.length) / 2);
 
     piece = {
         color: mino.color,
@@ -305,75 +245,119 @@ function setPiece(mino) {
             leftmost + (mino.shape.length - 1) / 2
         ]
     };
+
+    drawPiece();
 }
 
-let game = {
-    done: undefined,
-    level: 0,
-    lines: 0,
-    timer: 0,
-    speed: 2,
-    linesCleared: [],
-    placeBuffer: 0,
-    paused: false,
-    control: {
-        hdrop: {
-            pressed: 0,
-            buffer: 0
-        },
-        down: {
-            pressed: 0
-        },
-        left: {
-            pressed: 0
-        },
-        right: {
-            pressed: 0
-        },
-        ccw: {
-            pressed: 0,
-            buffer: 0
-        },
-        cw: {
-            pressed: 0,
-            buffer: 0
-        },
-        pause: {
-            pressed: 0,
-            buffer: 0
+function rotatePiece(dir, check = 0) {
+    erasePiece();
+
+    // If I just redefined the piece indices, the code breaks; I do not know why. 
+    let newIndices = [];
+
+    for (let i = 0; i < piece.indices.length; i++) {
+        // Move the x and y positions of the indices to (0, 0) instead of their current center
+        let y = piece.indices[i][0] - piece.center[0];
+        let x = piece.indices[i][1] - piece.center[1];
+        
+        // Calculate how far away they are from the center and what angle they're at from the center
+        let scale = Math.sqrt(y * y + x * x);
+        let angle = Math.atan2(x, y);
+
+        // Rotate the x and the y pieces clockwise or widdershins
+        let indexArray = [
+            Math.round(scale * Math.cos(angle - dir * Math.PI / 2) + piece.center[0]), 
+            Math.round(scale * Math.sin(angle - dir * Math.PI / 2) + piece.center[1])
+        ];
+
+        if (
+            // The attempted rotation location exists...
+            board[indexArray[0]][indexArray[1]] ||
+            // or if that location is out of bounds...
+            indexArray[0] < 0 || indexArray[0] > game.dims.height -1 ||
+            indexArray[1] < 0 || indexArray[1] > game.dims.width -1 ||
+            // or if it didn't pass TTC SRS checks...
+            check > 4
+        ) {
+            // Do not rotate
+            // Implement TTC SRS by adjusting the centerpoint, allowing for four extra checks
+            drawPiece();
+            return;
         }
-    },
-    config: { // separated from control to allow for later customization
-        hdrop: 'w',
-        left: 'a',
-        right: 'd',
-        down: 's',
-        ccw: 'j',
-        cw: 'k',
-        pause: 'Enter'
+
+        newIndices.push(indexArray);
     }
-};
+
+    piece.indices = newIndices;
+    drawPiece();
+}
+
+function movePiece(x, y) {
+    erasePiece();
+
+    let newIndices = [];
+
+    for (let i = 0; i < piece.indices.length; i++) {
+        let newY = piece.indices[i][0] + y;
+        let newX = piece.indices[i][1] + x;
+
+        // to-do: make it so that only downward collisions create a new piece
+
+        if (!board[newY] || board[newY][newX]) {
+            if (y != 0) {
+                if (game.placeBuffer) {
+                    for (let i = 0; i < piece.indices.length; i++) {
+                        board[piece.indices[i][0]][piece.indices[i][1]] = piece.color;
+                        if (!board[piece.indices[i][0]].includes(undefined)) {
+                            if (!game.linesCleared.includes(piece.indices[i][0])) {
+                                game.linesCleared.push(piece.indices[i][0]);
+                            }
+                        }
+                    }
+                    drawPiece();
+                    piece = undefined;
+                    game.placeBuffer--;
+                } else {
+                    game.placeBuffer = 1;
+                }
+            }
+            if (piece) drawPiece();
+            return;
+        } else if (newX < 0 || newX > game.dims.width - 1) {
+            drawPiece();
+            return;
+        }
+        newIndices.push([newY, newX]);
+    }
+
+    if (game.placeBuffer) game.placeBuffer--;
+
+    piece.indices = newIndices;
+    piece.center[0] += y;
+    piece.center[1] += x;
+
+    drawPiece();
+}
 
 document.addEventListener("keydown", (e) => {
-    for (let config in game.config) {
-        if (game.config[config] == e.key) {
-            if (!game.control[config].buffer) game.control[config].pressed = 1;
+    for (let key in game.control) {
+        if (game.control[key].key == e.key) {
+            if (!game.control[key].buffer) game.control[key].pressed = 1;
         }
     }
 });
 document.addEventListener("keyup", (e) => {
-    for (let config in game.config) {
-        if (game.config[config] == e.key) {
-            game.control[config] = {
-                pressed: 0,
-                buffer: game.control[config].buffer == undefined ? undefined : 0
-            };
+    for (let key in game.control) {
+        if (game.control[key].key == e.key) {
+            game.control[key].pressed = 0;
+            game.control[key].buffer = game.control[key].buffer == undefined ? undefined : 0;
         }
     }
 });
 
 let gameLoop = setInterval(() => {
     if (game.paused) {
+        // to do: rewrite as an event listener instead of something that's executed every frame
         if (game.control.pause.pressed) {
             game.control.pause.pressed = 0;
             
@@ -386,18 +370,22 @@ let gameLoop = setInterval(() => {
         }
     } else {
         if (game.linesCleared.length == 0) {
-            if (!piece) setPiece(minoArray[Math.floor(Math.random() * minoArray.length)]);
+            // If there is no piece, add one
+            if (!piece) setPiece(Math.floor(Math.random() * game.minoes.length));
 
+            // Increment the timer
             game.timer = (game.timer + 1) % 60;
 
+            // Move the piece down from time to time
             if (game.timer % Math.round(60 / game.speed) == 0) {
                 movePiece(0, 1);
             }
-
-            drawBoard();
             
+            // If the game is completed, stop the loop
             if (game.done) clearInterval(gameLoop);
 
+            // Control manager
+            // to do: make it so that this doesn't run if there isn't a piece (or, alternatively, move the piece down last)
             for (let control in game.control) {
                 if (game.control[control].pressed) {
                     game.control[control].pressed = 0;
@@ -426,7 +414,6 @@ let gameLoop = setInterval(() => {
                             rotatePiece(-1);
                             break;
                         case 'pause':
-                            eraseBoard();
                             pauseDiv.innerHTML = 'Paused!';
                             game.paused = true;
                             break;
@@ -434,21 +421,25 @@ let gameLoop = setInterval(() => {
                 }
             }
         } else {
+            // Sort line indices from smallest to largest; Ensures that no bugs happen when the above lines are dropped down
             game.linesCleared.sort((a, b) => (a - b));
+
             for (let i = 0; i < game.linesCleared.length; i++) {
+                // Clear line; No need to redraw since it's going to get overwritten anyway
                 board[game.linesCleared[i]].fill(undefined);
-                
+
+                // Move everything from above that line down
                 for (let j = game.linesCleared[i] - 1; j > -1; j--) {
-                    for (let k = 0; k < board[j].length; k++) board[j + 1][k] = board[j][k]; 
+                    for (let k = 0; k < board[j].length; k++) {
+                        board[j + 1][k] = board[j][k];
+                        drawPixel(j + 1, k, board[j + 1][k] || '#333333');
+                        drawPixel(j, k, '#333333');
+                    }
                     board[j].fill(undefined);
                 }
             }
-            drawBoard();
+
             game.linesCleared = [];
         }
     }
 }, 1000 / 60);
-
-/*
-add restart functionality
-*/
