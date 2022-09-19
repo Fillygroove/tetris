548150fb658fe8c8ff3f75fbb44ccaa5f129159f
Clarise Nicole Smith <36603779+Fillygroove@users.noreply.github.com>
2022-09-18 21:53:51 -0400

    Removed the background color dependencies

--- index.js
+++ index.js
@@ -41,38 +41,42 @@ class Piece {
                 pixel.className = 'game-col';
                 pixel.style.width = `${100 / config.dims.width}%`;
         
-                drawPixel(pixel, '#333333');
+                drawPixel(pixel, '');
             }
         }
     }
 
-    drawShadow() { // Find a way to rewrite this because WOW this is bad...
+    drawShadow() {
         if (config.pieceShadows) {
-            let escapeWhileLoop = true;
-            this.shadow = ((out = []) => {
+            let shadow = ((out = []) => {
                 for (let i = 0; i < this.indices.length; i++) out.push(this.indices[i].col);
                 return out;
             })();
+            let indices = this.indices;
 
-            while (escapeWhileLoop) {
+            (function dropLoop() {
                 let newIndices = [];
-                for (let i = 0; i < this.shadow.length; i++) {
-                    let newY = this.shadow[i] + 1;
 
-                    if (newY > config.dims.height - 1 || game.board[newY][this.indices[i].row]) {
-                        escapeWhileLoop = false;
-                        break;
-                    } else newIndices.push(newY);
+                for (let i = 0; i < shadow.length; i++) {
+                    let newY = shadow[i] + 1;
+
+                    if (newY > config.dims.height - 1 || game.board[newY][indices[i].row]) return;
+                    else newIndices.push(newY);
                 }
-                if (escapeWhileLoop) this.shadow = newIndices;
-            }
+
+                shadow = newIndices;
+
+                dropLoop();
+            })();
+
+            this.shadow = shadow;
 
             for (let i = 0; i < this.shadow.length; i++) {
                 let pixel = divBoard.children[this.shadow[i]].children[this.indices[i].row];
                 pixel.className = 'game-col';
                 pixel.style.width = `${100 / config.dims.width}%`;
         
-                drawPixel(pixel, shadeColor(this.indices[i].color, -80));
+                drawPixel(pixel, this.indices[i].color + '40');
             }
         }
     }
@@ -97,7 +101,7 @@ class Piece {
             pixel.className = 'game-col';
             pixel.style.width = `${100 / config.dims.width}%`;
     
-            drawPixel(pixel, '#333333');
+            drawPixel(pixel, '');
         }
     }
 
@@ -238,6 +242,7 @@ function shadeColor(color, percent) {
     var R = parseInt(color.substring(1,3),16);
     var G = parseInt(color.substring(3,5),16);
     var B = parseInt(color.substring(5,7),16);
+    var A = color.substring(7,9);
 
     R = parseInt(R * (100 + percent) / 100);
     G = parseInt(G * (100 + percent) / 100);
@@ -251,7 +256,7 @@ function shadeColor(color, percent) {
     var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
     var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
 
-    return "#"+RR+GG+BB;
+    return "#"+RR+GG+BB+A;
 }
 
 function drawPixel(pixel, color) {
@@ -260,17 +265,15 @@ function drawPixel(pixel, color) {
     pixel.style.backgroundColor = color;
 
     // Utter jank. I should base it off of tetromino index and not color.
-    if (color != '#333333') {
-        if (pixel.children.length == 0) {
-            let mino = document.createElement('div');
-            mino.className = 'game-mino';
-            mino.style.backgroundColor = color;
+    if (pixel.children.length == 0) {
+        let mino = document.createElement('div');
+        mino.className = 'game-mino';
+        mino.style.backgroundColor = color;
 
-            pixel.append(mino);
-        }
-
-        pixel.style.backgroundColor = shadeColor(color, -40);
+        pixel.append(mino);
     }
+
+    if (color != '') pixel.style.backgroundColor = shadeColor(color, -40);
 }
 
 function drawInDiv(div, minoID) {
@@ -929,7 +932,7 @@ gameInit({
     minos: [...standardMinos],
     garbage: {
         color: '#999999',
-        lines: 5,
+        lines: 8,
         holes: 6,
         erode: 2
     },
